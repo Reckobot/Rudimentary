df0e1b634ea3a37911190375c86e10a949d5ed65
Reckobot <140643581+Reckobot@users.noreply.github.com>
2025-02-11 13:33:05 -0500

    v4.4 (patch)

--- shaders/gbuffers_block.vsh
+++ shaders/gbuffers_block.vsh
@@ -1,1 +1,1 @@
-#include "/vertex/common.glsl"
+#include "/vertex/block.glsl"
--- shaders/lib/settings.glsl
+++ shaders/lib/settings.glsl
@@ -24,7 +24,7 @@
 #define FOLIAGE_CONTRAST 1.0 //[0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2.0]
 
 #define RESOLUTION_SCALE 3 //[1 2 3 4 5 6 7 8 9 10 11 12]
-#define DISTORTION 0.5 //[0 0.1 0.2 0.3 0.4 0.5 1.0 2.0 3.0 4.0 5.0 6.0 7.0 8.0]
+#define DISTORTION 0.5 //[0 0.1 0.2 0.3 0.4 0.5 0.75 1.0 2.0 3.0 4.0 5.0 6.0 7.0 8.0]
 
 #ifndef HORROR
     const vec3 sunColor = vec3(1.5,1.25,1)*0.75;
